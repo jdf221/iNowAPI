aa177598684bee46bddb5cdedbe6881911944af2
Jared Furlow <jdf2.furlow@gmail.com>
2018-04-02 17:49:21 -0500

    Clarity in niceTest.js

--- test/niceTest.js
+++ test/niceTest.js
@@ -15,15 +15,15 @@ const iNowAPI = require("../");
         console.log("Years", await Session.getYears());
         console.log("NineWeeks", await Session.getNineWeeks("29"));
 
-        let classesArray = await Session.getClasses(undefined, "75");
+        let classesArray = await Session.getClasses(undefined, "75"); //First param is year id, 2nd param is nineWeeksId
         console.log("Classes", classesArray);
 
         console.log(classesArray[2].name + " Assignments", await Session.getClassAssignments(classesArray[2].id));
 
         console.log("Demographics", await Session.getDemographics());
-        console.log("Attendance", await Session.getAttendance());
-        console.log("Period Attendance", await Session.getPeriodAttendance());
-        console.log("Check Ins and Check Outs", await Session.getCheckInOuts());
+        console.log("Attendance", await Session.getAttendance()); //Accepts a yearId
+        console.log("Period Attendance", await Session.getPeriodAttendance()); //Accepts a yearId
+        console.log("Check Ins and Check Outs", await Session.getCheckInOuts()); //Accepts a yearId
     }
     catch (error) {
         console.log("Error:", error);
